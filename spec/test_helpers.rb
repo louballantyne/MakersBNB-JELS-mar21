5d83ee8c126b143e07af7f13ff0150c7b852b3ac
Emmapr123 <emmapriester@hotmail.com>
2021-03-29 17:09:48 +0200

    started our listings class

---
+++ spec/test_helpers.rb
@@ -0,0 +1,8 @@
+# require 'pg'
+
+# def reset_table
+#   connection = PG.connect(dbname: 'makers_bnb_test')
+#   connection.exec("TRUNCATE TABLE  RESTART IDENTITY;")
+#   connection.exec("INSERT INTO  VALUES ();")
+#   connection.exec("INSERT INTO  () VALUES ()")
+# end 
---
+++ spec/unit/listings_spec.rb
@@ -0,0 +1,11 @@
+require 'listings'
+
+describe Listing do 
+  describe '.all' do 
+    
+  end 
+
+  describe '.create' do 
+    Listing.create()
+  end 
+end
