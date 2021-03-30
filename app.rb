3322531e1936e5d1d7bb8c69a056073bb4742a4d
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-03-30 14:43:50 +0100

    added some routes to app.rb

--- app.rb
+++ app.rb
@@ -2,11 +2,54 @@ require 'sinatra'
 
 class MakersBnb < Sinatra::Base
 
+  enable :sessions
 
-get '/' do
- erb(:index)
-end
+  get '/' do
+    erb(:'/listings/index')  
+  end
 
-run! if app_file == $0
+  get '/listings' do
+    @listings = Listing.all
+    @user = session[user_id]
+    erb(:'/listings/index')
+  end
 
+  get '/users/new' do
+    erb(:'/users/new')
+  end
+
+  post '/users/new' do
+    user = User.create(username: params[:username], email: params[:email], 
+                password: params[:password], name: params[:name])
+    session[user_id] = user.user_id
+    redirect('/listings/index')
+  end
+
+  get '/session/new' do
+    erb(:'/session/new')
+  end
+
+  post '/session/new' do
+    session[user_id] = user.user_id
+    # user = User.sign_in(username: params[:username], password: params[:password])
+    redirect('/listings/index')
+  end
+
+  get '/listing/:id' do
+    erb(:'/listing/index')
+  end
+
+  get '/listing/new' do
+    erb(:'/listing/new')
+  end
+
+  post '/listing/new' do
+    Listing.create(name: params[:name], country: params[:country], city: params[:city],
+                   sleeps: params[:sleeps], bedrooms: params[:bedrooms], 
+                   bathrooms: params[:bathrooms], description: params[:description], 
+                   type: params[:type], user_id: params[:user_id])
+    redirect('/listing/:id')
+  end
+
+  run! if app_file == $0
 end
