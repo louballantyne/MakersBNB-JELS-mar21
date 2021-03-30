0fcb7960f2984627f8baa50897be651a7babedb8
Emmapr123 <emmapriester@hotmail.com>
2021-03-30 12:35:06 +0200

    combined sql files

--- db/migrations/04_create_comments_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE comments (comment_id SERIAL PRIMARY KEY, 
-                       listing_id INTEGER, 
-                       user_id INTEGER, 
-                       comment VARCHAR(250), 
-                       timestamp TIMESTAMP, 
-                       FOREIGN KEY(listing_id) REFERENCES listings (listing_id), 
-                       FOREIGN KEY(user_id) REFERENCES users (user_id) 
-                       ON DELETE CASCADE);
