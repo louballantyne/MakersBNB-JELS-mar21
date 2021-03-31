5fef6b2138769860c3c1d18795cf5a718025a7e8
Louise Ballantyne <lousballantyne@icloud.com>
2021-03-31 10:22:32 +0100

    Fixed @user name

--- app.rb
+++ app.rb
@@ -10,9 +10,9 @@ class MakersBnb < Sinatra::Base
 
   enable :sessions , :method_override
 
-  before do 
+  before do
     check_env
-    @user = session[:user_id]
+    @user = User.find(session[:user_id])
   end
 
   get '/' do
@@ -21,6 +21,7 @@ class MakersBnb < Sinatra::Base
 
 
   get '/listings' do
+    @user = User.find(session[:user_id])
     @listings = Listing.all
     erb(:'/listings/index')
   end
@@ -30,9 +31,9 @@ class MakersBnb < Sinatra::Base
   end
 
   post '/users/new' do
-    @user = User.create(username: params[:username], email: params[:email],
+    user = User.create(username: params[:username], email: params[:email],
                 password: params[:password], name: params[:name])
-    session[:user_id] = @user.user_id
+    session[:user_id] = user.user_id
     redirect('/')
   end
 
@@ -69,9 +70,9 @@ class MakersBnb < Sinatra::Base
   end
 
   patch '/listing/:id' do
-    @listing = Listing.edit(listing_id: params[:id], name: params[:name], country: params[:country], 
-                            city: params[:city], sleeps: params[:sleep], bedrooms: params[:bedrooms], 
-                            bathrooms: params[:bathrooms], description: params[:description], 
+    @listing = Listing.edit(listing_id: params[:id], name: params[:name], country: params[:country],
+                            city: params[:city], sleeps: params[:sleep], bedrooms: params[:bedrooms],
+                            bathrooms: params[:bathrooms], description: params[:description],
                             type: params[:type])
     redirect("/listing/#{@listing.listing_id}")
   end
